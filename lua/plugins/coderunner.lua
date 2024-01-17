d8b3d5eb3e670aefe9c12f3431b3aff1836130d5
Marcos Grzesiak <marcosgrzesiak@gmail.com>
2024-01-17 17:30:41 -0500

    working coderunner setup

--- lua/plugins/coderunner.lua
+++ lua/plugins/coderunner.lua
@@ -1,8 +1,25 @@
-local opts = {
-    filetype = {
-        rust = { 'cargo run' },
-        typescript = 'bun run',
-    },
-}
+_CODE_RUNNER_TERM = 69420
 
-return { 'CRAG666/code_runner.nvim', opts = opts }
+local function run()
+    local cmd = require('code_runner.commands').get_filetype_command()
+    local term = require 'betterTerm'
+
+    print(vim.inspect(cmd))
+    term.open(_CODE_RUNNER_TERM)
+    term.select()
+    term.send(cmd, _CODE_RUNNER_TERM, { interrupt = false })
+end
+
+---@type LazySpec
+return {
+    'CRAG666/code_runner.nvim',
+    opts = {
+        filetype = {
+            rust = { 'cargo run' },
+            typescript = 'bun run',
+            go = { 'go run .' },
+        },
+    },
+    keys = { { '<leader>ru', run, desc = 'Run Code' } },
+    dependencies = { 'CRAG666/betterTerm.nvim' },
+}
