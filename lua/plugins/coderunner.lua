5357338aca6e03065e8ef2e91d67981d64e6199d
Marcos Grzesiak <marcosgrzesiak@gmail.com>
2024-01-22 20:50:56 -0500

    remove better term dependencies oooops

--- lua/plugins/coderunner.lua
+++ lua/plugins/coderunner.lua
@@ -1,15 +1,3 @@
-_CODE_RUNNER_TERM = 69420
-
-local function run()
-    local cmd = require('code_runner.commands').get_filetype_command()
-    local term = require 'betterTerm'
-
-    print(vim.inspect(cmd))
-    term.open(_CODE_RUNNER_TERM)
-    term.select()
-    term.send(cmd, _CODE_RUNNER_TERM, { interrupt = false })
-end
-
 ---@type LazySpec
 return {
     'CRAG666/code_runner.nvim',
@@ -20,6 +8,5 @@ return {
             go = { 'go run .' },
         },
     },
-    keys = { { '<leader>ru', run, desc = 'Run Code' } },
-    dependencies = { 'CRAG666/betterTerm.nvim' },
+    -- keys = { { '<leader>ru', run, desc = 'Run Code' } },
 }
