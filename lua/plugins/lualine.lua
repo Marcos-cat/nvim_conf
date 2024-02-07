7307426a2325ad5c07e53128fefbc442ba4fc814
Marcos Grzesiak <marcosgrzesiak@gmail.com>
2024-02-07 13:42:53 -0500

    add a lualine component for the lsps

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -10,23 +10,41 @@ local function macro_display()
     return '@' .. reg
 end
 
+local function lsps_display()
+    local lsps = vim.lsp.get_active_clients { bufnr = 0 }
+    for i, lsp in ipairs(lsps) do
+        lsps[i] = lsp.name
+    end
+
+    if #lsps == 0 then return '' end
+
+    return table.concat(lsps, ' '):gsub('language_server', 'ls')
+end
+
+local nvim_icon = {
+    filled = '',
+    unfilled = '',
+}
+
 return {
     'nvim-lualine/lualine.nvim',
     lazy = false,
     opts = {
         options = {
             icons_enabled = true,
-            theme = 'catppuccin-mocha',
-            component_separators = '|',
-            section_separators = '',
+            component_separators = { left = '', right = '' },
+            section_separators = { left = '', right = '' },
         },
         sections = {
-            lualine_a = { { 'mode' }, { macro_display } },
+            lualine_a = {
+                { 'mode', icon = nvim_icon.unfilled },
+                { macro_display },
+            },
             lualine_b = { { 'branch', icon = '' }, 'diff' },
             lualine_c = { 'diagnostics' },
 
             lualine_x = { { 'filename', path = path.relative } },
-            lualine_y = { 'filetype' },
+            lualine_y = { 'filetype', { lsps_display } },
             lualine_z = { 'progress' },
         },
     },
