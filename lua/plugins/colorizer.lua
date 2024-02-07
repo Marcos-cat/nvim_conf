d6a2ae36458e350d3a3be5718ceaee631228480a
Marcos Grzesiak <marcosgrzesiak@gmail.com>
2024-02-07 13:42:41 -0500

    make colorizer only work in noice windows (tooltips) and css

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,26 +1,6 @@
-local fts = {
-    'toml',
-    'lua',
-
-    'css',
-    'html',
-    'javascript',
-    'typescript',
-
-    'cpp',
-    'c',
-}
-
-local opts = {}
-local disabled = { names = false, RGB = false }
-
-for _, ft in ipairs(fts) do
-    opts[ft] = disabled
-end
-
 ---@type LazySpec
 return {
     'norcalli/nvim-colorizer.lua',
-    ft = fts,
-    opts = opts,
+    lazy = false,
+    opts = { 'noice', 'css' },
 }
