b60be1bed58765e7df6fe4f243685dc855b2e7eb
Marcos Grzesiak <marcosgrzesiak@gmail.com>
2024-02-07 13:42:09 -0500

    make oil not lazy, so it will takeover netwr tree

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,18 +1,13 @@
 local opts = {
     keymaps = { ['<leader>q'] = 'actions.close' },
-    float = {
-        max_width = 50,
-        max_height = 20,
-        win_options = {
-            winhighlight = 'Normal:Normal,FloatBorder:FloatBorder,FloatTitle:Normal',
-        },
-    },
+    float = { max_width = 50, max_height = 20 },
     delete_to_trash = true,
 }
 
 ---@type LazySpec
 return {
     'stevearc/oil.nvim',
+    lazy = false,
     opts = opts,
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     keys = {
