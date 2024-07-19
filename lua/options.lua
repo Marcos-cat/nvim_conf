d72efadc970e38b90b94e5c840e084b136b337a6
Marcos Grzesiak <marcosgrzesiak@gmail.com>
2024-07-19 02:18:53 -0400

    fix(opts): rearrange options and add `lil` filetype

--- lua/options.lua
+++ lua/options.lua
@@ -10,55 +10,41 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     pattern = '*',
 })
 
-vim.filetype.add { extension = { ua = 'uiua' } }
+vim.filetype.add { extension = { ua = 'uiua', lil = 'lil' } }
 
 vim.o.ignorecase = true
 vim.o.smartcase = true
 vim.o.foldmethod = 'manual'
-
--- Open vertical splits to the right
 vim.o.splitright = true
-
--- Set `completeopt` to have a better completion experience
-vim.o.completeopt = 'menuone,noselect'
-
+vim.o.splitbelow = true
+vim.opt.completeopt = { 'menuone', 'noselect' }
 vim.o.termguicolors = true
-
 vim.opt.sidescroll = 0
-
-vim.o.nu = true
+vim.o.number = true
 vim.o.relativenumber = true
-
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
-vim.opt.smartindent = true
-
-vim.opt.wrap = false -- Don't wrap lines
-
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
-vim.opt.undofile = true
-
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-
-vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = 'yes'
+vim.o.expandtab = true
+vim.o.tabstop = 4
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
+vim.o.smartindent = true
+vim.o.wrap = false -- Don't wrap lines
+vim.o.cursorline = true
+vim.o.cursorlineopt = 'line'
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.undofile = true
+vim.o.undodir = os.getenv 'HOME' .. '/.vim/undodir'
+vim.o.hlsearch = false
+vim.o.incsearch = true
+vim.o.termguicolors = true
+vim.o.scrolloff = 8
+vim.o.signcolumn = 'yes'
 vim.opt.isfname:append '@-@'
-
-vim.opt.updatetime = 50
-
-vim.opt.colorcolumn = '80' -- Visible column at 80 characters
-vim.opt.mouse = '' -- Disable mouse
-
-vim.opt.clipboard = 'unnamed'
-vim.opt.spell = true
-vim.opt.spelllang = 'en_us'
+vim.o.updatetime = 50
+vim.o.colorcolumn = tostring(80)
+vim.o.mouse = '' -- Disable mouse
+vim.o.clipboard = 'unnamed'
+vim.o.spell = true
+vim.o.spelllang = 'en_us'
 
 vim.lsp.set_log_level(4)
